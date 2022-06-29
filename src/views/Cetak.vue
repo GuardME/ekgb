14ccea25f8b148caba3280a1b78d6d92b6d9f858
RamadanYosiPratama <ramadanyosi30@gmail.com>
2022-06-29 11:22:12 +0700

    header cetak

--- src/views/Cetak.vue
+++ src/views/Cetak.vue
@@ -2,30 +2,26 @@
 
 <body style="background-color:#ccc">
 	<div class="preview-a4 p-1 my-1">
-		<div class="wrapper-rill" style="padding-top: 3px;">		
-			<div class="text-center my-3">
+        	<div class="text-center my-3">
 				<table width="100%" class="m-0">
 					<tr>
 						<td width="80px">
-							<img src="" width="140" class="ml-1">
+							<img src="@/assets/logo_psdkp_old.png" width="140" class="ml-1">
 						</td>
 						<td>
 							<table class="text-center" width="100%">
 								<tr>
 									<td colspan="4" height="50px">
 										<div class="bold title">
-											<h2 style="color: blue; font-size: 17.3px;">KEMENTERIAN KELAUTAN DAN PERIKANAN</h2>
-											<h2 style="color: blue; font-size: 19px; font-weight: normal;">DIREKTORAT JENDERAL PENGAWASAN</h2>
-											<h2 style="color: blue; font-size: 19px; font-weight: normal;">SUMBER DAYA KELAUTAN DAN PERIKANAN</h2>
-									
+											<h2 style="color: black; font-size: 17.3px; font-weight: bold;">KEMENTERIAN KELAUTAN DAN PERIKANAN</h2>
+											<h2 style="color: black; font-size: 19px; font-weight: bold;">DIREKTORAT JENDERAL PENGAWASAN</h2>
+											<h2 style="color: black; font-size: 19px; font-weight: bold;">SUMBER DAYA KELAUTAN DAN PERIKANAN</h2>
+										
 										</div>				
 										<div style="margin-top: 3px;">
-											<p class="text-center" style="font-size: 12px;">alamat<br>
-																				
-											21312}<br>
-											
-											Telepon / Faksimile: <br>
-											Laman: <i><a href="#">www.kkp.go.id</a></i>, Surat Elektronik: <i><a>{12321</a></i>
+											<p class="text-center" style="font-size: 12px;">JL. Medan Merdeka Timur No.16 Gedung Mina Bahari IV Lantai 11 Jakarta 10110<br>
+											Kotak Pos 4130 JKP 10041 Telepon (021) 3514728 (Hunting) Faksimile: (021) 3515484<br>
+											Website: <i><a href="#">www.djpsdkp.kkp.go.id</a></i>, E-mail: ditjenpsdkp@kkp.go.id<i><a></a></i>
 											</p>
 										</div>
 									</td>
@@ -35,123 +31,13 @@
 					</tr>
 					<tr>
 						<td colspan="2">
-						<hr style="border-top: 10px double #333;border-bottom: 1px double #333;">
+                            <hr>
+                            	<!-- <hr style="border-top: 10px double #333;border-bottom: 1px double #333;"></hr> -->
 						</td>
 					</tr>
-					<tr>
-						<td colspan="2">
-							<p class="text-right" style="margin-bottom: 10px;">1231}</p>
-							<h3 class="text-underline">SURAT KGB</h3>
-							<p class="text-center">Nomor : 12312</p>
-						</td>
-					</tr>
+				
 				</table>				
 			</div>
-
-			<div>
-				<table class="table my-1" style="text-algin: center; margin-left: 5px; width: 98%;">
-					<tr>
-						<td colspan="3">
-							<p style="margin-left: 25px;">Diberikan Cuti Tahunan untuk tahun TEST :</p>
-						</td>
-					</tr>
-					<tr>
-						<td colspan="3">&nbsp;</td>
-					</tr>
-					<tr>
-						<td width="150px" style="padding-top: 4px;">Nama</td>
-						<td width="10px" style="padding-top: 4px;">:</td>
-						<td style="padding-top: 4px;">nama dan gelar</td>
-					</tr>
-					<tr>
-						<td style="padding-top: 4px;">NIP</td>
-						<td style="padding-top: 4px;">:</td>
-						<td style="padding-top: 4px;">23132131</td>
-					</tr>
-					<tr>
-						<td style="padding-top: 4px;">Pangkat, Gol/ruang</td>
-						<td style="padding-top: 4px;">:</td>
-						<td style="padding-top: 4px;"><p>pangat</p></td>
-					</tr>
-					<tr>
-						<td style="padding-top: 4px;">Jabatan</td>
-						<td style="padding-top: 4px;">:</td>
-						<td style="padding-top: 4px;">nama jabatan</td>
-					</tr>
-					<tr>
-						<td style="padding-top: 4px;">Unit Kerja</td>
-						<td style="padding-top: 4px;">:</td>
-						<td style="padding-top: 4px;">nama unit kerja</td>
-					</tr>
-					<tr>
-						<td colspan="3">&nbsp;</td>
-					</tr>
-					<tr>
-						<td colspan="4">
-						
-							<p style="line-height: 1.5;">selama lama hari kerja, terhitung mulai  tanggal tgl dengan ketentuan sebagai berikut :</p>
-						</td>
-					</tr>
-					<tr>
-						<td colspan="3">
-							<ol style="list-style-type: lower-alpha;">
-								<li style="margin-bottom: 10px; padding-top: 4x; padding-left: 10px; line-height: 1.5;">
-									Sebelum menjalani cuti wajib menyerahkan pekerjaannya kepada atasan langsungnya.
-								</li>
-								<li style="padding-top: 4x; padding-left: 10px; line-height: 1.5;">
-									Setelah selesai menjalani cuti wajib melaporkan diri kepada atasan langsungnya dan bekerja kembali sebagaimana biasa.
-								</li>
-							</ol>
-						</td>
-					</tr>
-					<tr>
-						<td colspan="3">
-							<p style="margin-left: 25px;">Demikian surat izin cuti tahunan ini dibuat untuk dapat digunakan sebagaimana mestinya.</p>
-						</td>
-					</tr>
-				</table>				
-
-				<table class="table text-center my-3">
-					<tr>
-						<td width="280"></td>
-						<td></td>
-						<td width="280"></td>
-					</tr>
-					<tr>
-						<td width="280"></td>
-						<td></td>
-						<td width="280" style="text-align: left; margin-top: 4px; line-height: 1.5;">TEST</td>
-					</tr>
-					<tr>
-						<td colspan="3" height="80px"></td>
-					</tr>
-					<tr>
-						<td width="280" class="text-underline"></td>
-						<td></td>
-						<td width="280" style="text-align: left;">TEST</td>
-					</tr>
-					<tr>
-						<td width="280"></td>
-						<td></td>
-						<td width="280" style="text-align: left;"></td>
-					</tr>
-				</table>
-
-				<table class="table my=1" style="text-algin: center; margin-left: 5px; width: 98%;">
-					<tr>
-						<td width="280">Tembusan :</td>
-						<td></td>
-						<td width="280"></td>
-					</tr>
-					<tr>
-						<td width="280">Petugas Presensi</td>
-						<td></td>
-						<td width="280"></td>
-					</tr>
-				</table>
-			</div>
-
-		</div>
 	</div>
 </body>
 </template>
@@ -389,4 +275,6 @@ p {
 .bg-grey {
 	background-color: #bbb;
 }
+
+
 </style>
