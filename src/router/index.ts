c149387afee818551310f99517d3b3e31072e362
RamadanYosiPratama <ramadanyosi30@gmail.com>
2022-07-05 11:13:53 +0700

    Remove

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,7 @@ import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Layout from "@/views/Pages/Layout.vue"
 import Rinci from "@/views/Pages/Rinci/Rinci.vue"
-import Upload from "@/views/Upload.vue"
-import UploadForm from "@/views/Upload_Form.vue"
-import Cetak from "@/views/Cetak.vue"
 import Dashboard from "@/views/Pages/Dashboard/Dashboard.vue"
-import Search from "@/views/Search.vue"
-import DashdOld from "@/views/Dashboard.vue"
 import DashboardSK from "@/views/Pages/UpdateData/Dashboard/DashboardSK.vue"
 import RinciPegawai from "@/views/Pages/UpdateData/RinciPegawai/RinciPegawai.vue"
 import InputSK from "@/views/Pages/UpdateData/RinciPegawai/InputSK/InputSK.vue"
@@ -17,15 +12,6 @@ import SuccessAlert from "@/views/Pages/Alert/AlertSuccess.vue"
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  // {path: '/rincipegawai', component: RinciPegawai},
-  // {path: '/cetak', component: Cetak},
-  // {path: '/inputsk', component: InputSK},
-  // {path: '/rinci', component: Rinci},
-  // {path: '/upload', component: Upload},
-  // {path: '/uploadform', component: UploadForm},
-  // {path: '/dashboardOld', component: DashdOld},
- 
-  // {path: '/search', component: Search},
   {path: '', component: Layout,
     children: [
       {path: '', component: Dashboard},
